07e0a051d52c603aa5bed3d41d7ca28b05ccd101
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-02-13 16:16:58 -0600

    agrego menú

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Menu from './components/encabezado/menu/menu.jsx'
+
+function App() {
+  return (
+    <Menu />
+  );
+}
+
+export default App;
---
+++ src/components/encabezado/menu/menu.jsx
@@ -0,0 +1,14 @@
+import './menu.css'
+
+const Menu = () => {
+    return (
+        <>
+            <div className="menu">
+                <img className='menu__logo' src="https://i.ibb.co/2t97zXj/titulo.png" alt="Logotipo de la empresa" />
+                <button className='btn btn-outline-dark'>Haz tu cotización aquí</button>
+            </div>
+        </>
+    )
+}
+
+export default Menu
