7cc00636a16241d90dc82d189c41c1ed659c40e9
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-03-10 14:33:48 -0600

    de scss a bootstrap

--- src/components/encabezado/menu/menu.jsx
+++ src/components/encabezado/menu/menu.jsx
@@ -3,7 +3,7 @@ import './menu.css'
 const Menu = () => {
     return (
         <>
-            <div className="menu">
+            <div className="menu d-flex justify-content-around align-items-center">
                 <img className='menu__logo' src="./assets/titulo.png" alt="Logotipo de la empresa" />
                 <button className='btn btn-outline-dark'>Haz tu cotización aquí</button>
             </div>
