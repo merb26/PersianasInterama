1aff8f59a3f1a9d1c90fda9ff20096684ca5b28f
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-04-16 23:40:25 -0600

    agrego link con estilo en [Manuel E.]

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,9 @@ const Footer = () => {
     return (
         <>
             <div className="footer d-flex flex-column justify-content-between align-items-center">
-                <h5 className='m-5'>Desarrollado por Manuel E.</h5>
+
+                <h5 className='m-5'>Desarrollado por <a href="https://github.com/merb26" target="_blank" rel="noreferrer">Manuel E.</a></h5>
+
                 <p className='mb-1'>©2020 Interama - Derechos Reservados</p>
             </div>
         </>
